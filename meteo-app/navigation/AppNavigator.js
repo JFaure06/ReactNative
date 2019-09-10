93ccb0402780aee3ad52c7316ce51019cab45b71
Julien FAURE <julien.faure26@gmail.com>
2019-09-10 16:30:15 +0200

    mise en place de la TabNavigation

--- meteo-app/navigation/AppNavigator.js
+++ meteo-app/navigation/AppNavigator.js
@@ -1,13 +1,13 @@
-// AppNavigator.js
-
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from '../screens/HomeScreen';
 import AuthLoadingScreen from '../screens/AuthLoadingScreen';
 import IntroScreen from '../screens/IntroScreen';
 import IntroFormScreen from '../screens/IntroFormScreen';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-const AppStack = createStackNavigator({ Home: HomeScreen });
+//const AppStack = createStackNavigator({ Home: HomeScreen });
+const AppStack = createBottomTabNavigator({ Home: HomeScreen, SignIn: IntroFormScreen, Welcome: IntroScreen });
 const AuthStack = createStackNavigator({ SignIn: IntroFormScreen, Welcome: IntroScreen });
 
 export default createAppContainer(createSwitchNavigator(
@@ -20,3 +20,5 @@ export default createAppContainer(createSwitchNavigator(
         initialRouteName: 'AuthLoading',
     }
 ));
+
+//export default createAppContainer(AppStack);
